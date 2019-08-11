d08e051905594872ec1bf7fabf2de116ea089f6e
Madhumathii <52281595+Madhumathii@users.noreply.github.com>
2019-08-11 22:26:35 +0530

    Create madhumathi4.py

---
+++ madhumathi4.py
@@ -0,0 +1,12 @@
+a= int(input(" "))  
+if a> 1:  
+   for i in range(2,a):  
+       if (a% i) == 0:  
+           print(a,"is not a prime number")  
+           print(i,"times",a//i,"is",a)  
+           break  
+   else:  
+       print("yes")  
+         
+else:  
+   print("no")
